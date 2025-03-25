a90fc79874c76bb5612191ea5b5cc268e815ebdb
mrHoft <ru.hoft@gmail.com>
2025-03-25 22:52:28 +0300

    feat: reusable styles

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,12 +1,16 @@
+import styles from '~/styles/input.module.css';
+
 export default function Register() {
   return (
     <div>
       <h1>Register</h1>
       <form>
-        <input type="email" name="email" />
-        <input type="password" name="password" />
-        <input type="password" name="confirmPassword" />
-        <button type="submit">Register</button>
+        <input className={styles.input} type="email" name="email" placeholder="Email" />
+        <input className={styles.input} type="password" name="password" placeholder="Password" />
+        <input className={styles.input} type="password" name="confirmPassword" placeholder="Confirm password" />
+        <button className="button" type="submit">
+          Register
+        </button>
       </form>
     </div>
   );
