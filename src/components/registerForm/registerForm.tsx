530071ec12a6031d32b16c13e7f76be371647c30
Евгения Репникова <evrepni@gmail.com>
2025-03-26 00:07:57 +0500

    feat: add register form and route for it

---
+++ src/components/registerForm/registerForm.tsx
@@ -0,0 +1,13 @@
+export default function RegisterForm() {
+  return (
+    <div>
+      <h1>Register</h1>
+      <form>
+        <input type="email" name="email" />
+        <input type="password" name="password" />
+        <input type="password" name="confirmPassword" />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
