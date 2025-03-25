c268c8fb540a3c8015f471bc1f15c724c70ad7cc
Eugeniya <79003953+EugeniaRe@users.noreply.github.com>
2025-03-26 02:27:11 +0500

    feat: styles for register page (#10)
    * feat: add styles
    
    * feat: add styles for container
    
    * refactor: move styles from input.module to form.module

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,13 +1,13 @@
-import styles from '~/styles/input.module.css';
+import form from '~/styles/form.module.css';
 
 export default function Register() {
   return (
-    <div>
-      <h1>Register</h1>
-      <form>
-        <input className={styles.input} type="email" name="email" placeholder="Email" />
-        <input className={styles.input} type="password" name="password" placeholder="Password" />
-        <input className={styles.input} type="password" name="confirmPassword" placeholder="Confirm password" />
+    <div className={form.container}>
+      <h1 className={form.title}>Register</h1>
+      <form className={form.form}>
+        <input className={form.input} type="email" name="email" placeholder="Email" />
+        <input className={form.input} type="password" name="password" placeholder="Password" />
+        <input className={form.input} type="password" name="confirmPassword" placeholder="Confirm password" />
         <button className="button" type="submit">
           Register
         </button>
