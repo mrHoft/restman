7cfeeababc4d8c1db0ad80e5d6ff1cd10c36a740
Евгения Репникова <evrepni@gmail.com>
2025-03-26 02:24:37 +0500

    refactor: move styles from input.module to form.module

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,14 +1,13 @@
 import form from '~/styles/form.module.css';
-import styles from '~/styles/input.module.css';
 
 export default function Register() {
   return (
     <div className={form.container}>
       <h1 className={form.title}>Register</h1>
       <form className={form.form}>
-        <input className={styles.input} type="email" name="email" placeholder="Email" />
-        <input className={styles.input} type="password" name="password" placeholder="Password" />
-        <input className={styles.input} type="password" name="confirmPassword" placeholder="Confirm password" />
+        <input className={form.input} type="email" name="email" placeholder="Email" />
+        <input className={form.input} type="password" name="password" placeholder="Password" />
+        <input className={form.input} type="password" name="confirmPassword" placeholder="Confirm password" />
         <button className="button" type="submit">
           Register
         </button>
