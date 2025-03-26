bbb16335e44df538dfd3c64230617a1723f8166c
Евгения Репникова <evrepni@gmail.com>
2025-03-27 03:59:54 +0500

    chore: remove comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { Example } from '~/components/example/example';
 export default function Home() {
   return (
     <>
-      {/* <Header /> */}
       <main className="main">
         <Example />
       </main>
