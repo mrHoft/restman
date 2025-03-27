9566c4f1eaba36590bc6d5eb2c565b9bf33c397f
Евгения Репникова <evrepni@gmail.com>
2025-03-28 01:43:20 +0500

    feat: add error boundary

---
+++ src/app/error.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { useEffect } from 'react';
+import { Message } from '~/components/message/message';
+
+export default function Error({ error }: { error: Error & { digest?: string } }) {
+  useEffect(() => {
+    Message.show(`${error.name}: ${error.message}`, 'error');
+  }, [error]);
+
+  return;
+}
