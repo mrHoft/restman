0ae692edaeabd87ae4e3804456e54b5a357cdd3c
Евгения Репникова <evrepni@gmail.com>
2025-03-28 10:50:20 +0500

    feat: add and setup i18n, add dictionaries for main page

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,11 +1,14 @@
 import type { Metadata } from 'next';
+import { NextIntlClientProvider, hasLocale } from 'next-intl';
 import { Geist, Geist_Mono } from 'next/font/google';
+import { notFound } from 'next/navigation';
 import { Backdop } from '~/components/backdop/backdop';
 import { Footer } from '~/components/footer/footer';
 import { Header } from '~/components/header/header';
 import { Loader } from '~/components/loader/loader';
 import { Message } from '~/components/message/message';
 import { Modal } from '~/components/modal/modal';
+import { routing } from '~/i18n/routing';
 import { createClient } from '~/utils/supabase/server';
 
 import '~/styles/globals.css';
@@ -28,23 +31,34 @@ export const metadata: Metadata = {
   },
 };
 
-export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default async function RootLayout({
+  children,
+  params,
+}: Readonly<{ children: React.ReactNode; params: Promise<{ locale: string }> }>) {
   // TODO: move to middleware
   const supabase = await createClient();
   const {
     data: { user },
   } = await supabase.auth.getUser();
 
+  const { locale } = await params;
+
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
   return (
-    <html lang="en">
+    <html lang={locale}>
       <body className={`body ${geistSans.variable} ${geistMono.variable}`}>
-        <Backdop />
-        <Header user={user} />
-        <main className="main">{children}</main>
-        <Footer />
-        <Loader />
-        <Message />
-        <Modal />
+        <NextIntlClientProvider>
+          <Backdop />
+          <Header user={user} />
+          <main className="main">{children}</main>
+          <Footer />
+          <Loader />
+          <Message />
+          <Modal />
+        </NextIntlClientProvider>
       </body>
     </html>
   );
--- src/components/example/example.tsx
+++ src/components/example/example.tsx
@@ -7,10 +7,12 @@ import { Loader } from '../loader/loader';
 import Pagination from '../pagination/pagination';
 import { Select } from '../select/select';
 
+import { useTranslations } from 'next-intl';
 import styles from './example.module.css';
 
 export function Example() {
   const [page, setPage] = React.useState(0);
+  const t = useTranslations('MainPage');
 
   const handleShowLoader = () => {
     Loader.show();
@@ -21,22 +23,22 @@ export function Example() {
 
   return (
     <div className={styles.example}>
-      <h3>Component usage examples</h3>
+      <h3>{t('title')}</h3>
       <div className={styles.example__btns}>
         <button className="button" onClick={handleShowLoader}>
-          Show loader
+          {t('buttonForLoader')}
         </button>
         <button className="button" onClick={() => Modal.show(<h3>Modal example</h3>)}>
-          Show modal
+          {t('buttonForModal')}
         </button>
         <button className="button" onClick={() => Message.show('Example message', 'regular')}>
-          Show message
+          {t('buttonForMessage')}
         </button>
         <Select
-          options={['Option 1', 'Option 2', 'Option 3']}
+          options={[t('option1'), t('option2'), t('option3')]}
           name="select"
-          placeholder="Select option"
-          defaultValue="Option 1"
+          placeholder={t('selectorPlaceholder')}
+          defaultValue={t('option1')}
           onChange={value => console.log(value)}
         />
         <Pagination page={page} total={100} pageSize={10} onChange={setPage} />
