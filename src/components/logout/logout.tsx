b564f95945c2272c1c51091023815e7f4086ad24
AnthonyDavydchyk <62406462+AnthonyDavydchyk@users.noreply.github.com>
2025-03-28 21:33:37 +0300

    refactor: login redirect change to link

--- src/components/logout/logout.tsx
+++ src/components/logout/logout.tsx
@@ -1,13 +1,20 @@
-import { redirect } from 'next/navigation';
+import Link from 'next/link';
 import { signout } from '~/app/auth/actions';
 import type { UserData } from '~/utils/supabase/types';
 
 import styles from './logout.module.css';
 
 export function ButtonLogout({ user }: { user: UserData | null }) {
+  if (!user)
+    return (
+      <Link href="/login">
+        <button className={`${styles.sign} ${styles.in}`}>Login</button>
+      </Link>
+    );
+
   return (
-    <form action={user ? signout : () => redirect('/login')}>
-      <button className={`${styles.sign} ${user ? styles.out : styles.in}`}>{user ? 'Logout' : 'Login'}</button>
+    <form action={signout}>
+      <button className={`${styles.sign} ${styles.out}`}>Logout</button>
     </form>
   );
 }
