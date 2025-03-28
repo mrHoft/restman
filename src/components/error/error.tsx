3063bc5cf6eb76e3a970d6d0e2787c9df74ee445
Евгения Репникова <evrepni@gmail.com>
2025-03-28 13:45:27 +0500

    feat: add error message

--- src/components/error/error.tsx
+++ src/components/error/error.tsx
@@ -4,6 +4,9 @@ export default function Error({ error, reset }: { error: Error & { digest?: stri
   return (
     <div className={styles.error}>
       <h2>Something went wrong!</h2>
+      <div className={styles.error__name}>
+        {error.name}: {error.message}
+      </div>
       <textarea className={styles.error__stack} readOnly value={error.stack}></textarea>
       <button className="button" onClick={() => reset()}>
         Try again
