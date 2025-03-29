990df19e7e5291c5606c0988985bf695f6e91aaa
mrHoft <ru.hoft@gmail.com>
2025-03-29 10:20:58 +0300

    feat: prettifier

---
+++ src/utils/pretty.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+
+const getLineNumber = (i: number) => {
+  const n = (i + 1).toString();
+  const label = ' '.repeat(3 - n.length);
+  return <span style={{ color: '#888' }}>{`${label}${n}  `} </span>;
+};
+
+export function PrettyJson({ data }: { data: string }) {
+  const parsed = (() => {
+    try {
+      const json = JSON.parse(data);
+      const parsed = JSON.stringify(json, null, 2).split('\n');
+      return parsed;
+    } catch {
+      return null;
+    }
+  })();
+
+  if (!parsed) {
+    return <pre>{data}</pre>;
+  }
+
+  const prettifyLine = (line: string) => {
+    const regex = /"([^"]+)": /g;
+    const match = line.match(regex);
+    if (match) {
+      const index = line.indexOf(match[0]);
+      const space = ' '.repeat(index);
+      const rest = line.slice(index + match[0].length);
+      let restElement: React.ReactNode = rest;
+      if (rest.charCodeAt(0) >= 48 && rest.charCodeAt(0) <= 57) {
+        restElement = <span style={{ color: '#b5cea8' }}>{rest}</span>;
+      }
+      if (rest[0] === '"' || rest[0] === 'n') {
+        restElement = <span style={{ color: '#ce9178' }}>{rest}</span>;
+      }
+      return (
+        <>
+          {space}
+          <span style={{ color: '#9cdcfe' }}>{match[0]}</span>
+          {restElement}
+        </>
+      );
+    }
+    return line;
+  };
+
+  return (
+    <pre>
+      {parsed.map((line, i) => (
+        <div key={i}>
+          {getLineNumber(i)}
+          {prettifyLine(line)}
+        </div>
+      ))}
+    </pre>
+  );
+}
+
+export function PrettHtml({ data }: { data: string }) {
+  const lines = data.split('\n');
+  const prettifyLine = (line: string): React.ReactNode => {
+    const regex = /<([^>]+)>/g;
+    const match = line.match(regex);
+    if (match) {
+      const index = line.indexOf(match[0]);
+      const prev = line.slice(0, index);
+      const rest = line.slice(index + match[0].length);
+      const last = prettifyLine(rest);
+      return (
+        <>
+          {prev}
+          <span style={{ color: '#7cacf8' }}>{match[0]}</span>
+          {last}
+        </>
+      );
+    }
+    return line;
+  };
+
+  return (
+    <pre>
+      {lines.map((line, i) => (
+        <div key={i}>
+          {getLineNumber(i)}
+          {prettifyLine(line)}
+        </div>
+      ))}
+    </pre>
+  );
+}
+
+export function prettify(data: string) {
+  if (data.startsWith('{') || data.startsWith('[')) {
+    return { format: 'JSON', result: <PrettyJson data={data} /> };
+  }
+  if (data.startsWith('<')) {
+    return { format: 'HTML', result: <PrettHtml data={data} /> };
+  }
+  return { format: 'plain', result: <pre>{data}</pre> };
+}
