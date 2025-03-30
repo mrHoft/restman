cda89bdc578600c2c085653cfe790afca4a1cf8f
Евгения Репникова <evrepni@gmail.com>
2025-03-31 00:57:02 +0500

    feat: add locale

--- src/app/[locale]/[method]/[url]/[body]/page.tsx
+++ src/app/[locale]/[method]/[url]/[body]/page.tsx
@@ -2,18 +2,19 @@ export default async function Page({
   params,
   searchParams,
 }: {
-  params: Promise<{ method: string; url: string; body: string }>;
+  params: Promise<{ locale: string; method: string; url: string; body: string }>;
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
-  const { method, url, body } = await params;
+  const { locale, method, url, body } = await params;
   const search = await searchParams;
 
   return (
     <div>
+      <div>Locale: {locale}</div>
       <div>Method: {method}</div>
       <div>Url: {url}</div>
       <div>Body: {body}</div> <div>Search: {JSON.stringify(search)}</div>
-      {/* <Client method={method} url={url} body={body} searchParams={search}/> */}
+      {/* <Client locale={locale} method={method} url={url} body={body} searchParams={search}/> */}
     </div>
   );
 }
--- src/app/[locale]/[method]/[url]/page.tsx
+++ src/app/[locale]/[method]/[url]/page.tsx
@@ -2,17 +2,18 @@ export default async function Page({
   params,
   searchParams,
 }: {
-  params: Promise<{ method: string; url: string }>;
+  params: Promise<{ locale: string; method: string; url: string }>;
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
-  const { method, url } = await params;
+  const { locale, method, url } = await params;
   const search = await searchParams;
 
   return (
     <div>
+      <div>Locale: {locale}</div>
       <div>Method: {method}</div>
       <div>Url: {url}</div> <div>Search: {JSON.stringify(search)}</div>
-      {/* <Client method={method} url={url}  searchParams={search}/> */}
+      {/* <Client locale={locale} method={method} url={url}  searchParams={search}/> */}
     </div>
   );
 }
