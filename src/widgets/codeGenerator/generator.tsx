130bacbb328d6659bde342a9814aed390df9d270
mrHoft <ru.hoft@gmail.com>
2025-03-31 04:21:29 +0300

    feat: code-generator

---
+++ src/widgets/codeGenerator/generator.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import React from 'react';
+import { Select } from '~/components/select/select';
+import { generateCode, type TRuntime } from '~/utils/generator';
+
+const options: TRuntime[] = ['curl', 'fetch', 'xhr', 'node', 'go', 'python', 'java', 'csharp'];
+
+interface GeneratorProps {
+  method: string;
+  url: string;
+  body: string;
+  headers: Record<string, string>;
+}
+
+export function CodeGenerator({ method, url, body, headers }: GeneratorProps) {
+  const [runtime, setRuntime] = React.useState<TRuntime>('curl');
+
+  const handleChange = (value: string) => {
+    setRuntime(value as TRuntime);
+  };
+
+  return (
+    <details>
+      <summary style={{ cursor: 'pointer' }}>Generate code</summary>
+      <div style={{ width: '12rem' }}>
+        <Select options={options} name="select" placeholder="runtime" defaultValue={runtime} onChange={handleChange} />
+      </div>
+      <pre>
+        <code style={{ whiteSpace: 'pre-wrap' }}>{generateCode(runtime, method, url, body, headers)}</code>
+      </pre>
+    </details>
+  );
+}
