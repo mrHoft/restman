b03e085fb9bf92733d1389e635309091abab19b8
Eugeniya <79003953+EugeniaRe@users.noreply.github.com>
2025-04-02 22:58:09 +0500

    feat: add page variables (#49)
    * feat: add variables
    
    * refactor: remove unused code
    
    * refactor: add classname and hover for remove button, remove isClient, change implementing saving variables to LS
    
    * refactor: rename variables to vars
    
    * chore: remove comments

--- src/entities/useVariables.ts
+++ src/entities/useVariables.ts
@@ -5,7 +5,8 @@ import Storage from '~/utils/storage';
 const storage = new Storage();
 const setVariable = (name: string, value: string) => storage.set(`variables.${name}`, value);
 const getVariables = () => storage.get<Record<string, string>>('variables');
+const setAllVariables = (value: Record<string, string>) => storage.set('variables', value);
 
 export default function useVariables() {
-  return { setVariable, getVariables };
+  return { setVariable, getVariables, setAllVariables };
 }
---
+++ src/widgets/variables/variables.tsx
@@ -0,0 +1,98 @@
+'use client';
+
+import { useEffect, useMemo, useState } from 'react';
+import useVariables from '~/entities/useVariables';
+
+import styles from './variables.module.css';
+
+const initialVariablesState = [{ '': '' }];
+
+export default function Variables() {
+  const { setVariable, getVariables, setAllVariables } = useVariables();
+
+  const [vars, setVars] = useState<Record<string, string>[]>(initialVariablesState);
+
+  useEffect(() => {
+    const storedVariables = getVariables();
+    if (storedVariables) {
+      const newVariables = Object.entries(storedVariables).map(([name, value]) => ({ [name]: value }));
+      setVars(() => [...newVariables, { '': '' }]);
+    }
+  }, []);
+
+  const handleAdd = () => setVars(prev => [...prev, { '': '' }]);
+
+  const variablesTable = useMemo(() => {
+    const handleNameChange = (i: number, value: string, name: string) => {
+      const newVariables = [...vars];
+      newVariables[i] = { [name]: value };
+      setVars(() => newVariables);
+
+      const result = createResult(newVariables);
+      setAllVariables(result);
+    };
+
+    const handleValueChange = (i: number, name: string, value: string) => {
+      setVariable(name, value);
+      const newVariables = [...vars];
+      newVariables[i] = { [name]: value };
+      setVars(() => newVariables);
+    };
+
+    const handleRemove = (index: number) => {
+      const newVariables = vars.filter((_, i) => i !== index);
+      setVars(newVariables);
+      const result = createResult(newVariables);
+      setAllVariables(result);
+    };
+
+    return (
+      <>
+        {vars.map((el, index) => {
+          const name = Object.keys(el)[0];
+          const value = Object.values(el)[0];
+          return (
+            <div key={index} className={styles.variables__row}>
+              <input
+                type="text"
+                placeholder="Name"
+                value={name}
+                onChange={e => handleNameChange(index, value, e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder="Value"
+                value={value}
+                onChange={e => handleValueChange(index, name, e.target.value)}
+              />
+              <button className={styles.variables__remove} onClick={() => handleRemove(index)}>
+                <img src="/icons/cross.svg" alt="delete" />
+              </button>
+            </div>
+          );
+        })}
+      </>
+    );
+  }, [vars, setVars]);
+
+  const createResult = (items: Record<string, string>[]) =>
+    items.reduce<Record<string, string>>((acc, item) => {
+      const name = Object.keys(item)[0];
+      if (name === '') return acc;
+      acc[name] = item[name];
+      return acc;
+    }, {});
+
+  return (
+    <div className={styles.variables}>
+      <h3 className={styles.variables__title}>Variables</h3>
+      {variablesTable}
+      <div className={styles.variables__row}>
+        <div className={styles.variables__item}></div>
+        <div className={styles.variables__add} onClick={handleAdd}>
+          Add
+        </div>
+      </div>
+    </div>
+  );
+}
