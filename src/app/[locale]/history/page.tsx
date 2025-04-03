bea17bb46beb68083a57f158e57e530430f713a1
Евгения Репникова <evrepni@gmail.com>
2025-04-03 23:30:31 +0500

    feat: add pagination

--- src/app/[locale]/history/page.tsx
+++ src/app/[locale]/history/page.tsx
@@ -1,6 +1,6 @@
 import History from '~/widgets/history/history';
 
-export default async function HistoryPage({ params }: { params: Promise<{ locale: string; slug?: string[] }> }) {
+export default async function PageHistory({ params }: { params: Promise<{ locale: string; slug?: string[] }> }) {
   const { locale } = await params;
   return <History locale={locale} />;
 }
--- src/widgets/history/history.tsx
+++ src/widgets/history/history.tsx
@@ -1,37 +1,76 @@
 'use client';
 
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useEffect, useMemo, useState } from 'react';
 import useHistory, { type HistoryRecord } from '~/entities/useHistory';
 
 import styles from './history.module.css';
 
+export const ItemsForPage = 10;
+
 export default function History({ locale }: { locale: string }) {
   const { getHistory } = useHistory();
-
   const [history, setHistory] = useState<HistoryRecord[]>([]);
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const lastPage = Math.ceil(history.length / ItemsForPage);
+  const currentPage = Number(searchParams.get('page')) || 1;
+  const lastItemIndex = useMemo(() => currentPage * ItemsForPage, [currentPage]);
+  const firstItemIndex = useMemo(() => lastItemIndex - ItemsForPage, [lastItemIndex]);
+  const currentHistory = useMemo(() => history.slice(firstItemIndex, lastItemIndex), [history, currentPage]);
 
   useEffect(() => {
     const storedHistory = getHistory();
     if (storedHistory) {
-      const sortedHistory = storedHistory && storedHistory.sort((a, b) => b.date - a.date);
-      setHistory(() => [...sortedHistory]);
+      const newHistory = storedHistory.sort((a, b) => b.date - a.date);
+      setHistory(newHistory);
     }
-  }, []);
+  }, [getHistory]);
+
+  const Pagination = useMemo(() => {
+    const handlePrev = () => {
+      router.push(`${pathname}?page=${currentPage - 1}`);
+    };
+    const handleNext = () => {
+      router.push(`${pathname}?page=${currentPage + 1}`);
+    };
+    return (
+      <div className={styles.history__pagination}>
+        <button
+          disabled={currentPage === 1}
+          className={`${styles.history__page} ${styles.prev} `}
+          onClick={handlePrev}
+        ></button>
+        <span>
+          {currentPage} / {lastPage}
+        </span>
+        <button
+          disabled={currentPage === lastPage}
+          className={`${styles.history__page} ${styles.next}`}
+          onClick={handleNext}
+        ></button>
+      </div>
+    );
+  }, [currentPage, history, pathname, router]);
 
   return (
     <div className={styles.history}>
       <h2 className={styles.history__title}>History Requests</h2>
 
-      {history.length > 0 ? (
-        history.map((item, index) => (
-          <div key={index} className={styles.history__item}>
-            <div>{item.method}</div>
-            <Link className={styles.history__url} href={`${item.url}`}>
-              {item.url}
-            </Link>
-          </div>
-        ))
+      {currentHistory.length > 0 ? (
+        <>
+          {currentHistory.map((item, index) => (
+            <div key={index} className={styles.history__item}>
+              <div>{item.method}</div>
+              <Link className={styles.history__url} href={`${item.url}`}>
+                {item.url}
+              </Link>
+            </div>
+          ))}
+          {Pagination}
+        </>
       ) : (
         <div className={styles.history__empty}>
           History is empty. It&apos;s time to{' '}
