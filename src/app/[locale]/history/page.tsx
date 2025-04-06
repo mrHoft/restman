842af289e9e379a0979ef0a496c24da80ea479f4
Евгения Репникова <evrepni@gmail.com>
2025-04-06 22:01:12 +0500

    fix: history - change dinamic to lazy

--- src/app/[locale]/history/page.tsx
+++ src/app/[locale]/history/page.tsx
@@ -1,14 +1,11 @@
-import dynamic from 'next/dynamic';
+import { lazy } from 'react';
 import { getUser } from '~/app/auth/actions';
-import { Welcome } from '~/components/welcome/welcome';
 import type { Locale } from '~/i18n-config';
-import { getDictionary } from '../dictionaries';
 
-const History = dynamic(() => import('~/widgets/history/history'));
+const History = lazy(() => import('~/widgets/history/history'));
 
 export default async function PageHistory({ params }: { params: Promise<{ locale: Locale; slug?: string[] }> }) {
   const { locale } = await params;
   const user = await getUser();
-  const dict = await getDictionary(locale);
-  return user ? <History locale={locale} /> : <Welcome dict={dict.MainPage} />;
+  return <>{user && <History locale={locale} />}</>;
 }
