44843d5e38f34e499c6b86edf7047b6998852040
Eugeniya <79003953+EugeniaRe@users.noreply.github.com>
2025-04-16 11:55:49 +0500

    feat: tests utils (#97)
    * feat: base64 tests
    
    * feat: cookie tests
    
    * feat: pretty tests
    
    * feat: rest tests
    
    * chore: remove cookie and storage tests
    
    * feat: teste for schema
    
    * feat: tests clients
    
    * feat: tests supabase server
    
    * feat: tests supabase middleware
    
    * feat: tests generator
    
    * feat: add snapshots to gitignore
    
    * chore: remove toMatchSnapshot method
    
    * fix: return tests with toMatchSnapshot
    
    * fix: rewrite generator tests, remove toMatchSnapshot
    
    * fix: rewrite tests for generator
    
    * fix: remove screen, add and remove empty lines, remove NextURL, fix pretty tests
    
    * refactor: add empty lines
    
    * chore: remove whatwg-fetch
    
    * refactor: change toHaveLength to length.toBe(3)

---
+++ src/utils/supabase/clients.test.ts
@@ -0,0 +1,27 @@
+import { createBrowserClient } from '@supabase/ssr';
+import { createClient } from './clients';
+
+jest.mock('@supabase/ssr', () => ({
+  createBrowserClient: jest.fn(),
+}));
+
+const mockCreateBrowserClient = createBrowserClient as jest.Mock;
+
+describe('createClient', () => {
+  const originalEnv = process.env;
+  const mockUrl = 'http://mock.supabase.co';
+  const mockAnonKey = 'mock-anon-key';
+
+  jest.clearAllMocks();
+  process.env = {
+    ...originalEnv,
+    NEXT_PUBLIC_SUPABASE_URL: mockUrl,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: mockAnonKey,
+  };
+
+  it('should call createBrowserClient with correct environment variables', () => {
+    createClient();
+
+    expect(mockCreateBrowserClient).toHaveBeenCalledWith(mockUrl, mockAnonKey);
+  });
+});
---
+++ src/utils/supabase/middleware.test.ts
@@ -0,0 +1,93 @@
+/**
+ * @jest-environment node
+ */
+
+import { createServerClient } from '@supabase/ssr';
+import { NextRequest } from 'next/server';
+import { updateSession } from './middleware';
+
+jest.mock('@supabase/ssr', () => ({
+  createServerClient: jest.fn(),
+}));
+
+const mockAuth = { getUser: jest.fn() };
+const mockSupabase = { auth: mockAuth };
+(createServerClient as jest.Mock).mockReturnValue(mockSupabase);
+const mockGetUser = mockAuth.getUser;
+
+const createMockRequest = (pathname: string, cookies = {}) => {
+  const origin = 'http://example.com';
+  const urlString = `${origin}${pathname}`;
+  const url = new URL(urlString);
+  const request = new Request(urlString);
+  const cookieStore = {
+    getAll: () => Object.entries(cookies).map(([name, value]) => ({ name, value })),
+    set: jest.fn(),
+  };
+  const nextUrlMock: Partial<NextRequest['nextUrl']> = {
+    pathname: url.pathname,
+    searchParams: url.searchParams,
+    origin: url.origin,
+    toString: () => url.toString(),
+    clone: () => {
+      const clonedUrl = new URL(url.origin);
+      return clonedUrl;
+    },
+  };
+  const mockRequest = {
+    ...request,
+    cookies: cookieStore,
+    nextUrl: nextUrlMock as NextRequest['nextUrl'],
+  } as unknown as NextRequest;
+
+  return mockRequest;
+};
+
+describe('middleware', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should redirect unauthorized user from protected route', async () => {
+    mockGetUser.mockResolvedValue({ data: { user: null } });
+    const request = createMockRequest('/client');
+    const response = await updateSession(request);
+
+    expect(response.headers.get('location')).toBe('http://example.com/');
+  });
+
+  it('should redirect authorized user from auth route', async () => {
+    mockGetUser.mockResolvedValue({ data: { user: { email: 'user@mail.com' } } });
+    const request = createMockRequest('/login');
+    const response = await updateSession(request);
+
+    expect(response.headers.get('location')).toBe('http://example.com/');
+  });
+
+  it('should allow access to protected routes for authorized user ', async () => {
+    mockGetUser.mockResolvedValue({ data: { user: { email: 'user@mail.com' } } });
+    const request = createMockRequest('/client');
+    const response = await updateSession(request);
+
+    expect(response.status).toBe(200);
+  });
+
+  it('should allow access to auth routes for unauthorized user', async () => {
+    mockGetUser.mockResolvedValue({ data: { user: null } });
+    const request = createMockRequest('/register');
+    const response = await updateSession(request);
+
+    expect(response.status).toBe(200);
+  });
+
+  it('should call cookies setAll', async () => {
+    mockGetUser.mockResolvedValue({ data: { user: { email: 'user@mail.com' } } });
+    const request = createMockRequest('/client', { cookie: 'test' });
+    await updateSession(request);
+    const mockSetAll = (createServerClient as jest.Mock).mock.calls[0][2].cookies.setAll;
+    const cookiesToSet = [{ name: 'example-name', value: 'example-value' }];
+    mockSetAll(cookiesToSet);
+
+    expect(request.cookies.set).toHaveBeenCalled();
+  });
+});
